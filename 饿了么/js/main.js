f801174ae74bb39e9e51dc1840f1b7820bc5ca2f
sagomm <wwangtao731@gmail.com>
2016-05-16 16:22:20 -0400

    添加了新的样式

--- 饿了么/js/main.js
+++ 饿了么/js/main.js
@@ -43,9 +43,9 @@ app.template = {
         shopIconArr: [],
         // 得到整个商店模板
         getShop: function (img, time, title, loaction, sale, startCost, cost, iconArr) {
-            var shopIcons;
-            for (i in this.shopIconArr) {
-                shopIcons += shopIconArr;
+            var shopIcons = undefined;
+            for (i in iconArr) {
+                shopIcons += this.shopIconArr[iconArr[i]];
             }
             var left = '<div　class="shop"><div class="left">' + this.shopImg(img) + this.shopTime(time) + '</div>';
             var right = '<div class="right">' + this.shopTitle(title, location) + '<div class="star-sales"><span class="star-base icon icon-star"><i class="icon icon-star"></i></span>' + this.shopSales(sale) + '</div>' + shopTake(startCost, cose) + '<div class="icons">' + shopIcons + '</div></div>';
@@ -123,7 +123,7 @@ app.picBanner = (function () {
 
 app.shop = (function () {
     var shops = document.getElementById('shops');
-    function shopTemplate() {
+    function setShopIcon() {
         /**定义商铺中显示的图标 */
         app.template.shop.shopAddIcon('减','<i style="background:#f07373;">减</i>');
         app.template.shop.shopAddIcon('首','<i style="background:#70bc46;">首</i>');
@@ -132,14 +132,21 @@ app.shop = (function () {
         app.template.shop.shopAddIcon('票','<i style="background:#fff;color:#9071CB;border:1px solid;padding:0;">票</i>'); 
         app.template.shop.shopAddIcon('保','<i style="background:#fff;color:#4B9A18;border:1px solid;padding:0;">保</i>');
         app.template.shop.shopAddIcon('赔','<i style="background:#fff;color:#FF4E00;border:1px solid;padding:0;">赔</i>');
-        /**返回整个商铺的模板 */
-        return app.template.shop.getShop();         
     }
-    function 
-    
-    
-    
-
+    /**
+     * logo 商标图片
+     * time 配送时间
+     * title 商铺名称
+     * location 商铺位置
+     * sale 一个月售出的的单数
+     * startCost 起送价格
+     * cost 配送的费用
+     * iconArr 商铺下面的标记以及它的说明，出现在对shop,hover之后
+     */
+    function shop(logo,time,title,location,sale,startCost,cost,iconArr,shopIconInfo) {
+        this.shopTemplate = '';
+        this.shop
+    }
 })();
 
 
