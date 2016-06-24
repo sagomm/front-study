13b48b6cde80da8098279eee35fdb631cc7b2a29
sagomm <348371873@qq.com>
2016-06-24 07:43:37 -0400

    6.24

--- Tmall/js/main.js
+++ Tmall/js/main.js
@@ -22,17 +22,17 @@ var Common = {
 }
 
 // 控制商品分类以及狂欢会场的hover
-// var shopClassify = document.getElementById('extra-shopClassify');
-// var meeting = document.getElementById('extra-meeting');
+var shopClassify = document.getElementById('extra-shopClassify');
+var meeting = document.getElementById('extra-meeting');
 
-// shopClassify.addEventListener('mouseover',function (e) {
-//     Common.addClass(this,'extra-shopClassify-active');
-//     Common.removeClass(meeting,'extra-meeting-active');
-// })
-// meeting.addEventListener('mouseover',function (e) {
-//     Common.addClass(this,'extra-meeting-active');
-//     Common.removeClass(shopClassify,'extra-shopClassify-active');
-// })
+shopClassify.addEventListener('mouseover',function (e) {
+    Common.addClass(this,'extra-shopClassify-active');
+    Common.removeClass(meeting,'extra-meeting-active');
+})
+meeting.addEventListener('mouseover',function (e) {
+    Common.addClass(this,'extra-meeting-active');
+    Common.removeClass(shopClassify,'extra-shopClassify-active');
+})
 
 // 幻灯片部分
 
