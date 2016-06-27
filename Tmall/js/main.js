6561fc631078a7376f5ddda010704acd3d137735
sagomm <348371873@qq.com>
2016-06-27 04:17:52 -0400

    6.27

--- Tmall/js/main.js
+++ Tmall/js/main.js
@@ -21,18 +21,18 @@ var Common = {
     }
 }
 
-// 控制商品分类以及狂欢会场的hover
-var shopClassify = document.getElementById('extra-shopClassify');
-var meeting = document.getElementById('extra-meeting');
+// // 控制商品分类以及狂欢会场的hover
+// var shopClassify = document.getElementById('extra-shopClassify');
+// var meeting = document.getElementById('extra-meeting');
 
-shopClassify.addEventListener('mouseover',function (e) {
-    Common.addClass(this,'extra-shopClassify-active');
-    Common.removeClass(meeting,'extra-meeting-active');
-})
-meeting.addEventListener('mouseover',function (e) {
-    Common.addClass(this,'extra-meeting-active');
-    Common.removeClass(shopClassify,'extra-shopClassify-active');
-})
+// shopClassify.addEventListener('mouseover',function (e) {
+//     Common.addClass(this,'extra-shopClassify-active');
+//     Common.removeClass(meeting,'extra-meeting-active');
+// })
+// meeting.addEventListener('mouseover',function (e) {
+//     Common.addClass(this,'extra-meeting-active');
+//     Common.removeClass(shopClassify,'extra-shopClassify-active');
+// })
 
 // 幻灯片部分
 
@@ -111,7 +111,7 @@ setInterval(function(){
 
 // 显示侧边栏
 function showLift() {
-    
+                        
 }
 // 
 
